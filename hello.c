f8fdff522d85dff416d381690e1bea0bb6b3a8e9
cmikulicz <cmikulicz@outlook.com>
2019-01-17 14:40:40 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Christian Mikulicz
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Christian Mikulicz\n");
+
+  return 0;
+}
